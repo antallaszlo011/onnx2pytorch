6f449461fc060bb5d5c3ec4ad7731ebf6e3e05ad
Zhouxing Shi <zhouxingshichn@gmail.com>
2024-05-25 16:49:08 -0700

    Use Mul+ReduceSum for MatMul when weight is 1D

--- onnx2pytorch/operations/matmul.py
+++ onnx2pytorch/operations/matmul.py
@@ -4,4 +4,7 @@ from torch import nn
 
 class MatMul(nn.Module):
     def forward(self, A, V):
-        return torch.matmul(A, V)
+        if V.ndim == 1:
+            return (A * V).sum(dim=-1)
+        else:
+            return torch.matmul(A, V)
